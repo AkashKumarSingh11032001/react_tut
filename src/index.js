da9bd2aac6c8375c6f5535f86c33730dea03fa7a
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-06-11 08:49:12 +0530

    index

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css'
 // const newName = names.map(x => {return <h1>{x}</h1>})
 // console.log(newName);
 
-// accessing data using list of objects
+// accessing data using list of objects []list of books details
 const BooksDetails = [
     {
         img: "https://www.thebalancecareers.com/the-parts-of-book-content-2799968",
@@ -26,38 +26,47 @@ const BooksDetails = [
     }
 ];
 
-const firstBook = {
-    img: "https://www.thebalancecareers.com/the-parts-of-book-content-2799968",
-    title: "Happy World",
-    author: "Akash Singh"
-}
-const secondBook = {
-    img: "https://collins.co.uk/pages/times-books",
-    title: "Thinkk before you speak",
-    author: "Ak Singh"
-}
-const thirdBook = {
-    img: "https://www.thespruce.com/how-to-sell-used-books-1388970",
-    title: "Dont lie to me",
-    author: "Akash"
-}
+// init phase 1. {seprate books contents}
+// const firstBook = {
+//     img: "https://www.thebalancecareers.com/the-parts-of-book-content-2799968",
+//     title: "Happy World",
+//     author: "Akash Singh"
+// }
+// const secondBook = {
+//     img: "https://collins.co.uk/pages/times-books",
+//     title: "Thinkk before you speak",
+//     author: "Ak Singh"
+// }
+// const thirdBook = {
+//     img: "https://www.thespruce.com/how-to-sell-used-books-1388970",
+//     title: "Dont lie to me",
+//     author: "Akash"
+// }
 
 const BookList = (props) => {
     return (
         <div className="booklist">
-            <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
+            {/* old ways */}
+            {/* <Book img={firstBook.img} title={firstBook.title} author={firstBook.author} />
             <Book img={secondBook.img} title={secondBook.title} author={secondBook.author} />
-            <Book img={thirdBook.img} title={thirdBook.title} author={thirdBook.author} />
+            <Book img={thirdBook.img} title={thirdBook.title} author={thirdBook.author} /> */}
+            
+            {/* new ways using ES6 properties*/}
+            {BooksDetails.map(x => { // x is single object from Booksdetails
+                return <Book book={x} />
+                
+            })}
         </div>
     );
 }
 
-const Book = (props) => {
+const Book = (props) => { // props will recive an object 'book' and inside that main containe aviable.
+    const {img, title, author} = props.book;
     return (
         <div className="book">
-            <Image img={props.img} />
-            <Title title={props.title} />
-            <Author author={props.author} />
+            <Image img={img} />
+            <Title title={title} />
+            <Author author={author} />
         </div>
     );
 }
