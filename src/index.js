8f976e30bcd114fa98af8a51a7dab7a2e1398cdb
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-06-11 07:35:25 +0530

    index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,36 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-function App(){
-    return <h1>Hello World</h1>;
+
+const BookList = () => {
+    return (
+        <div>
+            <Book />
+        </div>
+    );
 }
 
-ReactDom.render(<App/>,document.getElementById('root'));
+const Book = () => {
+    return (
+        <div>
+            <Image />
+            <Title />
+            <Author />
+        </div>
+    );
+}
+
+const Image = () => {
+    return <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVzbKcUuxCtRDgrOphFFV9koAIh9iSyy13BtuvtVgxdNKjJhfF1X1v6V1IwpX8zmH-R0c&usqp=CAU" alt="APG" />
+};
+const Title = () => {
+    return <h2>The taste of Life.</h2>
+};
+
+const Author = () => {
+    return <h4>APG Abdul Klam sir</h4>
+}
+
+
+
+ReactDom.render(<BookList />, document.getElementById('root'));
