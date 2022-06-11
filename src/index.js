a0364f8953b2137273a1dc9883d4321648403645
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-06-11 07:57:47 +0530

    props

--- src/index.js
+++ src/index.js
@@ -5,24 +5,18 @@ import './index.css'
 const BookList = () => {
     return (
         <div className="booklist">
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book title="See you soon"/>
+            <Book title="The taste of Life."/>
+            <Book title="Happy world"/>
         </div>
     );
 }
 
-const Book = () => {
+const Book = (props) => {
     return (
         <div className="book">
             <Image />
-            <Title />
+            <Title title={props.title} />
             <Author />
         </div>
     );
@@ -31,8 +25,8 @@ const Book = () => {
 const Image = () => {
     return <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVzbKcUuxCtRDgrOphFFV9koAIh9iSyy13BtuvtVgxdNKjJhfF1X1v6V1IwpX8zmH-R0c&usqp=CAU" alt="APG" />
 };
-const Title = () => {
-    return <h1>The taste of Life.</h1>
+const Title = (props) => {
+    return <h1>{props.title}</h1>
 };
 
 const Author = () => {
